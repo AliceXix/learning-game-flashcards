2f91ec41ff8594a13d8b0d7b49f47bd8b9749e03
Alice <alice.jost@outlook.de>
2021-10-08 15:26:26 +0200

    feature: close on clear desk

--- endgame.js
+++ endgame.js
@@ -31,6 +31,7 @@ export function gameEnd () {
 
    endAll.onclick = function () {
        startGame() === false
+       window.open('', '_self').close()
    }
 
     restartAll.onclick = function () {
